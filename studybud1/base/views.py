29c6a4be0d0b0c20cd81ae3283f45ccc015ea7cf
Closdlgdo <cidstp031921@gmail.com>
2024-01-13 04:39:14 -0700

    We are creating a discord-like application with Python Django thanks to: Dennis Ivy from Python Django 7 Hour Course on YT

---
+++ studybud1/base/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+# studybud1/base/views.py
+# They are functions or classes
+# When someone goes to a specific URL, they will be sent here. Any queries to the DB will be sent here.
