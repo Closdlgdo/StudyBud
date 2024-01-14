98e1c982547bd81d943ab5afb004f1e69cd30c36
Closdlgdo <cidstp031921@gmail.com>
2024-01-14 07:22:19 -0700

    We are creating a discord-like application with Python Django thanks to: Dennis Ivy from Python Django 7 Hour Course on YT

--- studybud1/base/admin.py
+++ studybud1/base/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Room
+from .models import Room, Topic, Message
 
 admin.site.register(Room)  # register the model with django
+admin.site.register(Topic) # register the model with django
+admin.site.register(Message) # register the model with django
