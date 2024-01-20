afbce0bfa1257eb1951216875daf5f90dbb529cd
Closdlgdo <cidstp031921@gmail.com>
2024-01-20 06:40:26 -0700

    We are creating a discord-like application with Python Django thanks to: Dennis Ivy from Python Django 7 Hour Course on YT

--- studybud1/base/views.py
+++ studybud1/base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -17,7 +18,14 @@ from .forms import RoomForm
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') is not None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q)
+        | Q(name__icontains=q)
+        | Q(description__icontains=q)
+        # | Q(host__username__icontains=q)
+        # | Q(host__email__icontains=q)
+        # | Q(topic__name__icontains=q)
+    )
 
     topics = Topic.objects.all()
 
