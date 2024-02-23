a498d6dec371e99749cb9fa8a24d8cfea264bb15
Closdlgdo <cidstp031921@gmail.com>
2024-02-23 05:54:37 -0700

    We are creating a discord-like application with Python Django thanks to: Dennis Ivy from Python Django 7 Hour Course on YT

--- studybud1/base/views.py
+++ studybud1/base/views.py
@@ -185,3 +185,9 @@ def deleteMessage(request, pk):
         return redirect("home")
 
     return render(request, "base/delete.html", {'obj': message})
+
+
+@login_required(login_url="/login")
+def updateUser(request):
+
+    return render(request, "base/update_user.html", {"form": form})
