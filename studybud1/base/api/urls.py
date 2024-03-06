194b634ba511148172540e7f0ed8ee29833ac6e6
Closdlgdo <cidstp031921@gmail.com>
2024-03-06 04:39:26 -0700

    We are creating a discord-like application with Python Django thanks to: Dennis Ivy from Python Django 7 Hour Course on YT

--- studybud1/base/api/urls.py
+++ studybud1/base/api/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.getRoutes, name="routes"),
     path("rooms/", views.getRooms, name="rooms"),
-    path("room/<str:pk>", views.getRoom, name="room"),
+    path("rooms/<str:pk>", views.getRoom, name="room"),
 ]
