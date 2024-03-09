74f3bf91722a7d5fef586916f85e2544827c2f4b
Closdlgdo <cidstp031921@gmail.com>
2024-03-09 05:30:24 -0700

    We are creating a discord-like application with Python Django thanks to: Dennis Ivy from Python Django 7 Hour Course on YT

--- studybud1/base/models.py
+++ studybud1/base/models.py
@@ -4,43 +4,43 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
-#
-# # Create your models here.
-# # This is where we configure our Database
-#
-#
-# class Topic(models.Model):
-#     name = models.CharField(max_length=200)
-#
-#     def __str__(self):
-#         return self.name
-#
-#
-# class Room(models.Model):
-#     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-#     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
-#     name = models.CharField(max_length=200)
-#     description = models.TextField(null=True, blank=True)
-#     participants = models.ManyToManyField(User, related_name='participants', blank=True)
-#     updated = models.DateTimeField(auto_now=True)
-#     created = models.DateTimeField(auto_now_add=True)
-#
-#     class Meta:
-#         ordering = ['-updated', '-created']
-#
-#     def __str__(self):
-#         return self.name
-#
-#
-# class Message(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)  # when a room gets deleted, all the messages are deleted too.
-#     room = models.ForeignKey(Room, on_delete=models.CASCADE)
-#     body = models.TextField()
-#     updated = models.DateTimeField(auto_now=True)
-#     created = models.DateTimeField(auto_now_add=True)
-#
-#     class Meta:
-#         ordering = ['-updated', '-created']
-#
-#     def __str__(self):
-#         return self.body[0:50]
+
+# Create your models here.
+# This is where we configure our Database
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # when a room gets deleted, all the messages are deleted too.
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.body[0:50]
